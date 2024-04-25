c227c7e2260ffe087d23fa38f97808219b5f45af
Sweta Rai <165440537+shweta-rai11@users.noreply.github.com>
2024-04-25 15:47:43 -0600

    Add files via upload

---
+++ clustering-1/code_to_download.R
@@ -0,0 +1,34 @@
+library(GEOquery)
+
+# Download existing RA datasets
+# Reference: 	GSE13837
+
+# Download GSE13837
+gse <- getGEO("GSE13837")
+class(gse)
+show(gse)
+show(pData(phenoData(gse[[1]]))[1:4,])
+class(gse[[1]])
+
+# The expression set is a first element of the list 
+eset <- gse[[1]]
+head(pData(eset))
+class(eset)
+# Save expression set
+save( eset, file="GSE13837_ESet.RData" )
+
+# Save the expression matrix
+write.csv(exprs(eset), file="GSE13837_expressions.csv")
+
+# Get sample data
+write.csv(pData(eset), file="GSE13837_phenodata.csv")
+
+# Get annotation
+gpl <- getGEO(eset@annotation)
+class(gpl)
+head(gpl@dataTable@table)
+annotation <- gpl@dataTable@table
+head(annotation)[1:3,]
+# Save annotation
+write.csv(annotation, file="GSE13837_annotation.csv")
+
